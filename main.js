60142184aa035a273cfca779f30b956c4091975d
hellish666glory <svyatos10@gmail.com>
2024-08-24 12:35:34 +0600

    fixed validation errors, added phone mask

--- main.js
+++ main.js
@@ -14,6 +14,44 @@ document.addEventListener('DOMContentLoaded', () => {
     const thxBlock = document.querySelector('.thank-you__modal')
     revealElement(cookie);
 
+    const maskedInputs = document.querySelectorAll("[data-mask]");
+
+    for (let index = 0; index < maskedInputs.length; index++) {
+        maskedInputs[index].addEventListener('input', maskInput);
+    }
+
+    function maskInput() {
+        let input = this;
+        let mask = input.dataset.mask;
+        let value = input.value;
+        const literalPattern = /[0\*]/;
+        const numberPattern = /[0-9]/;
+        let newValue = "";
+        try {
+            let maskLength = mask.length;
+            let valueIndex = 0;
+            let maskIndex = 0;
+
+            for (; maskIndex < maskLength;) {
+            if (maskIndex >= value.length) break;
+
+            if (mask[maskIndex] === "0" && value[valueIndex].match(numberPattern) === null) break;
+
+            // Found a literal
+            while (mask[maskIndex].match(literalPattern) === null) {
+                if (value[valueIndex] === mask[maskIndex]) break;
+                newValue += mask[maskIndex++];
+            }
+            newValue += value[valueIndex++];
+            maskIndex++;
+            }
+
+            input.value = newValue;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     closeModal.addEventListener('mouseup', () => {
         hideElement(modal);
         hideElement(modalBackdrop);
